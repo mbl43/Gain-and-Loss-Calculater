5b08e2fd32faacfa5a0837288fdfd0e3e631b2c6
mangesh lemte <mangeshlemtea23@gmail.com>
2024-03-05 08:23:19 +0530

    all the bug and issues are fixed

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function autoRefresh()
+{
+        window.location.reload();
+}
+setInterval(autoRefresh, 60000);
+
 function calc()
 {
     
@@ -10,36 +16,48 @@ function calc()
         document.getElementById('results').innerHTML = "Please enter valid numbers.";
         return;
     }
-
-    if(sell>buy)
+    else if(buy === sell)
     {
-        alert("you are calculating profit in percentage");
-        let profit = ((sell * quantity) - (buy * quantity)) / (buy * quantity) * 100;
-
-        document.getElementById("results").innerHTML = `You made profit on capital: <span class="text"> ${profit.toFixed(2)}</span>%`;
+         document.getElementById("results").innerHTML="NO Profit-No Loss"
+    }
+    else if(sell>buy)
+        {
+         alert("profit");
+        let pro= (sell * quantity) - (buy * quantity)
+        let profit = ( pro) / (buy * quantity) * 100;
+        
+        document.getElementById("results").innerHTML = `Profit In %: <span class="text">  ${profit.toFixed(2)}&#37;</span>`;
+        document.getElementById("results2").innerHTML = `Profit In INR : <span class="text">  ${pro.toFixed(2)}₹</span>`;
         return;
 
     }
-    let capital=buy*quantity;
-    let loss=capital-(sell*quantity)
-    let capital_after_loss=capital-loss;
-    let loss_per=capital*loss/100;
-   
-    let gain=loss*100/capital_after_loss
-    console.log("buy:", buy,"quantity:",quantity,"sellprice:",sell,"capital:",capital,"capital after loss:",capital_after_loss,"loss in percentage:",  ` ${loss_per}%` );
-    console.log("need gain on capital:",`${gain}%`);
-
-    if(gain>100)
+    else
     {
-    console.log("You need multibagger stock ");
-    }
-    document.getElementById("results").innerHTML = `Need a gain to recover your Loss On Remaining Capital: <span class="text"> ${gain.toFixed(2)}</span>%`;
-
-
+        let capital=buy*quantity;
+        let loss=capital-(sell*quantity)
+        let capital_after_loss=capital-loss;
+        let loss_per=capital*loss/100;
+    
+        let gain=loss*100/capital_after_loss
+        console.log("buy:", buy,"quantity:",quantity,"sellprice:",sell,"capital:",capital,"capital after loss:",capital_after_loss,"loss in percentage:",  ` ${loss_per}%` );
+        console.log("need gain on capital:",`${gain}%`);
+        
+        if(gain>=100)
+        {
+            console.log("You need multibagger stock ");
+        }
+        document.getElementById("results").innerHTML =`Need a gain to recover your Loss On Remaining Capital:
+        <span class="text">${gain.toFixed(2)}</span>&#37;`;
+        document.getElementById("results2").innerHTML =`
+        Loss In INR: <span class="text">${loss.toFixed(2)} </span>₹`;
+        
+    
+}
 }
 function clearInputs() {
     document.querySelector('.buy-price').value = "";
     document.querySelector('.quantity').value = "";
     document.querySelector('.sell-price').value = "";
     document.getElementById('results').innerHTML = ""; // Clear results
+    document.getElementById('results2').innerHTML = ""; // Clear results
 }
